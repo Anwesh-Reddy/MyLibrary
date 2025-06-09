9632b5452dd222b7f36fd3554f966f46ce6c1182
arkotla <anweshreddy36@gmail.com>
2025-06-08 19:19:09 -0700

    Added all requests for the project

--- src/main/java/com/library/project/Impl/BookServiceImpl.java
+++ src/main/java/com/library/project/Impl/BookServiceImpl.java
@@ -6,6 +6,7 @@ import com.library.project.Service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,5 +22,39 @@ public class BookServiceImpl implements BookService {
     @Override
     public Optional<BookEntity> getBookByIsbn(Long isbn) {
         return bookRepository.findById(isbn);
+    }
+
+    @Override
+    public BookEntity saveBook(BookEntity bookEntity) {
+        return bookRepository.save(bookEntity);
+    }
+    @Override
+    public BookEntity updateBook(BookEntity bookEntity, Long isbn) {
+        Optional<BookEntity> existingBookOptional = bookRepository.findById(isbn);
+
+        if (existingBookOptional.isPresent()) {
+            BookEntity existingBook = existingBookOptional.get();
+            existingBook.setName(bookEntity.getName());
+            existingBook.setAisle(bookEntity.getAisle());
+            existingBook.setShelf(bookEntity.getShelf());
+            return bookRepository.save(existingBook);
+        }
+        return null;
+    }
+
+    @Override
+    public BookEntity deleteBook(Long isbn) {
+        Optional<BookEntity> bookOptional = bookRepository.findById(isbn);
+
+        if (bookOptional.isPresent()) {
+        BookEntity book = bookOptional.get();
+        bookRepository.deleteById(isbn);
+        return book;
+        }
+        return null;
+    }
+
+    public List<BookEntity> getAllBooks() {
+        return bookRepository.findAll();
     }
 }
