12d1327d9aa3b308e230f6a7bfba9192df068f39
Bhavya Sree Chinthala <bhavyasreechinthala@Bhavyas-MacBook-Air.attlocal.net>
2025-06-13 01:45:01 -0500

    Updated BookController with ResponseStatus for POST and PUT, DB config

--- src/main/java/com/library/project/Controller/BookController.java
+++ src/main/java/com/library/project/Controller/BookController.java
@@ -4,6 +4,7 @@ import com.library.project.Entity.BookEntity;
 import com.library.project.Impl.BookServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,18 +28,20 @@ public class BookController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public BookEntity createBook(@RequestBody BookEntity bookEntity){
-        return bookServiceImpl.saveBook(bookEntity);
+    public ResponseEntity<BookEntity> createBook(@RequestBody BookEntity bookEntity){
+        BookEntity savedBook = bookServiceImpl.saveBook(bookEntity);
+        return new ResponseEntity<>(savedBook, HttpStatus.CREATED);
     }
 
     @PutMapping("/update/{isbn}")
-    public BookEntity updateBook(@RequestBody BookEntity bookEntity, @PathVariable Long isbn){
-        return bookServiceImpl.updateBook(bookEntity, isbn);
+    public ResponseEntity<BookEntity> updateBook(@RequestBody BookEntity bookEntity, @PathVariable Long isbn) {
+        BookEntity updatedBook = bookServiceImpl.updateBook(bookEntity, isbn);
+        return ResponseEntity.ok(updatedBook);
     }
 
     @DeleteMapping("/delete/{isbn}")
-    public BookEntity deleteBook(@PathVariable Long isbn){
-        return bookServiceImpl.deleteBook(isbn);
+    public ResponseEntity<BookEntity> deleteBook(@PathVariable Long isbn){
+        BookEntity deletedBook = bookServiceImpl.deleteBook(isbn);
+        return ResponseEntity.ok(deletedBook);
     }
 }
